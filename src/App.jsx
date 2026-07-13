e40722395f30342161ffcac3791acc8d76bdaf8b
AshishPG31 <ashishpanchal731@gmail.com>
2026-07-13 12:32:02 +0530

    Initial portfolio setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="bg-[#030712] min-h-screen text-white">
+      <h1 className="text-5xl font-bold text-center pt-20">
+        Ashish.dev
+      </h1>
+
+      <p className="text-center text-gray-400 mt-5">
+        Portfolio Under Development 🚀
+      </p>
+    </div>
+  );
+}
+
+export default App;
